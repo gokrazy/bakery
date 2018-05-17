c9f6615c39c4f47ffc9a5e990cd410615b2235db
Michael Stapelberg <michael@stapelberg.de>
2018-05-17 09:07:56 +0200

    bake: wait for /sys/class/net/eth0 to appear

--- cmd/bake/bake.go
+++ cmd/bake/bake.go
@@ -7,12 +7,21 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func testMacAddress() error {
-	b, err := ioutil.ReadFile("/sys/class/net/eth0/address")
-	if err != nil {
-		return err
+	var b []byte
+	for i := 0; i < 10; i++ {
+		var err error
+		b, err = ioutil.ReadFile("/sys/class/net/eth0/address")
+		if err != nil && !os.IsNotExist(err) {
+			return err
+		}
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(i) * time.Second)
 	}
 	if !strings.HasPrefix(string(b), "b8:27:eb:") {
 		return fmt.Errorf("MAC address %q does not start with b8:27:eb: (Raspberry Pi Foundation)", string(b))
