4ee7309a33e5e172cb31ac9d32cf8e34b0f934b4
Michael Stapelberg <michael@stapelberg.de>
2022-03-04 21:15:22 +0100

    bake: test unencrypted and encrypted wifi
    fixes https://github.com/gokrazy/gokrazy/issues/110

--- cmd/bake/bake.go
+++ cmd/bake/bake.go
@@ -2,12 +2,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/gokrazy/gokrazy"
+	"github.com/vishvananda/netlink"
 )
 
 var (
@@ -62,7 +68,121 @@ func testUSB() error {
 	return nil
 }
 
+func removeWifiAddresses() error {
+	link, err := netlink.LinkByName("wlan0")
+	if err != nil {
+		return err
+	}
+	addrs, err := netlink.AddrList(link, netlink.FAMILY_V4)
+	if err != nil {
+		return err
+	}
+	for _, addr := range addrs {
+		if err := netlink.AddrDel(link, &addr); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func testWifi(ssid, psk string) error {
+	ctx, canc := context.WithCancel(context.Background())
+	defer canc()
+
+	disconnect := func() {
+		log.Printf("disconnecting wlan0 if present")
+		disconnect := exec.Command("/user/wifi", "-disconnect")
+		disconnect.Stdout = os.Stdout
+		disconnect.Stderr = os.Stderr
+		if err := disconnect.Run(); err != nil {
+			log.Print(err)
+		}
+	}
+	disconnect()
+	defer disconnect()
+	time.Sleep(1 * time.Second)
+
+	if err := removeWifiAddresses(); err != nil {
+		log.Printf("%T, %v", err, err)
+	}
+
+	start := time.Now()
+	timeout := 30 * time.Second
+	go func() {
+		log.Printf("starting wifi program for network %q (timeout: %v)", ssid, timeout)
+		for time.Since(start) < timeout {
+			if ctx.Err() != nil {
+				break // context canceled
+			}
+			wifi := exec.CommandContext(ctx, "/user/wifi",
+				"-ssid="+ssid,
+				"-psk="+psk)
+			wifi.Stdout = os.Stdout
+			wifi.Stderr = os.Stderr
+			if err := wifi.Run(); err != nil {
+				log.Print(err)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		_, err := ioutil.ReadFile("/sys/class/net/wlan0/address")
+		if err != nil && !os.IsNotExist(err) {
+			return err
+		}
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(i) * time.Second)
+	}
+
+	_, dhcpNet, err := net.ParseCIDR("192.168.1.0/24")
+	if err != nil {
+		return err
+	}
+
+	log.Printf("wlan0 interface appeared, awaiting DHCP IP address (%v)", dhcpNet)
+
+	start = time.Now()
+	for time.Since(start) < timeout {
+		intf, err := net.InterfaceByName("wlan0")
+		if err != nil {
+			return err
+		}
+		addrs, err := intf.Addrs()
+		if err != nil {
+			return err
+		}
+		for idx, addr := range addrs {
+			log.Printf("wlan0 addr %d: %T, %s", idx, addr, addr)
+			if ipnet, ok := addr.(*net.IPNet); ok {
+				if dhcpNet.Contains(ipnet.IP) {
+					if err := removeWifiAddresses(); err != nil {
+						return err
+					}
+					return nil // wifi test succeeded
+				}
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return fmt.Errorf("no address out of %v found on wlan0 after %v", dhcpNet, timeout)
+}
+
+func testUnencryptedWifi() error {
+	return testWifi("gokrazy-cinet-open", "")
+}
+
+func testEncryptedWifi() error {
+	// This network is only powered on when the gokrazy CI is running, and is
+	// not connected to anything. It’s okay to leak its WPA-PSK here.
+	return testWifi("gokrazy-cinet-psk", "ur7ieSeinu")
+}
+
 func main() {
+	gokrazy.WaitForClock()
+
 	result := "SUCCESS\n"
 
 	if err := testMacAddress(); err != nil {
@@ -71,6 +191,21 @@ func main() {
 
 	if err := testUSB(); err != nil {
 		result = fmt.Sprintf("FAILURE: testUSB: %v\n", err)
+	}
+
+	model := gokrazy.Model()
+	if strings.HasPrefix(model, "Raspberry Pi 4 Model B Rev ") ||
+		strings.HasPrefix(model, "Raspberry Pi 3 Model B Rev ") ||
+		strings.HasPrefix(model, "Raspberry Pi 3 Model B Plus Rev ") {
+		if err := testUnencryptedWifi(); err != nil {
+			result = fmt.Sprintf("FAILURE: testUnencryptedWifi: %v\n", err)
+		}
+
+		time.Sleep(1 * time.Second)
+
+		if err := testEncryptedWifi(); err != nil {
+			result = fmt.Sprintf("FAILURE: testEncryptedWifi: %v\n", err)
+		}
 	}
 
 	log.Print(result)
--- go.mod
+++ go.mod
@@ -4,11 +4,19 @@ go 1.17
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.3.5
-	github.com/gokrazy/internal v0.0.0-20200626090505-539bb61868de
+	github.com/gokrazy/gokrazy v0.0.0-20220304072708-5dd8496371e3
+	github.com/gokrazy/internal v0.0.0-20220111191949-698f19a074ef
 	github.com/gokrazy/updater v0.0.0-20210106211705-4d92b338dd24
+	github.com/vishvananda/netlink v1.1.0
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158
 )
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/google/renameio/v2 v2.0.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mdlayher/watchdog v0.0.0-20201005150459-8bdc4f41966b // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	rsc.io/goversion v1.2.0 // indirect
+)
